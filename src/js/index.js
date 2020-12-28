ce08261d1a9bfc7519799ef86d300a6fe04719a1
Christiano0407 <placebo0704@gmail.com>
2020-12-28 07:30:30 -0600

    data-metodos_1

--- src/js/index.js
+++ src/js/index.js
@@ -6,40 +6,59 @@ console.log(array);  */
 //
 class MyArray{
     constructor(){
+        //num de elementos.
         this.length = 0; 
+        //elementos que voy a guardar. 
         this.data = {}; 
     }
+    //index=agregar
     get(index){
     return this.data[index]; 
     }
+    //item = llamar. 
+    //Agregar elementos
     push(item){
         this.data[this.length] = item; 
         this.length++; 
         return this.data; 
     }
+    //Quitar el último elemento. 
     pop(item){
         const lastItem = this.data[this.length - 1]; 
         delete this.data[this.length -1]; 
         this.length--; 
         return lastItem; 
     }
+    //Borrar un elemento. 
     delete(index){
         const item = this.data[index]
         this.shiftIndex(index);
         return item; 
     }
     shiftIndex(index){
+        //No iniciar desde 0, se pone index 
         for(let i = index; i < this.length -1; i++){
             this.data[i] = this.data[i + 1]; 
         }
         delete this.data[this.length -1]; 
         this.length--; 
     }
+    //Elimina el primer elemento. 
     shift(){
         const firstItem = this.data[0];
         this.shiftIndex(0);
         return firstItem; 
     }
+    //Agrega un elemento al inicio del array. 
+    unshift (item) {
+        for (let i = this.length; i > 0; i--) {
+            this.data[i] = this.data[i - 1];
+        }
+        this.data[0] = item;
+        this.length++;
+        return this.data;
+    }
+
 }
 const oneArray = new MyArray(); 
 console.log(oneArray.push("Pammela")); 
@@ -53,4 +72,6 @@ console.log(oneArray.length);
 console.log(oneArray.delete(1));
 console.log(oneArray); 
 console.log(oneArray.shift()); 
+console.log(oneArray); 
+console.log(oneArray.unshift("Samantha"));
 console.log(oneArray); 
