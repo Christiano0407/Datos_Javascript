17672c549cd797c6f57442797180b1484cdbfed1
Christiano0407 <placebo0704@gmail.com>
2020-12-29 09:08:20 -0600

    previous-doubleLinked

--- src/js/doubleLinked.js
+++ src/js/doubleLinked.js
@@ -25,6 +25,39 @@ class MyDoubleLinkedList {
     this.length++;
     return this; 
     }
+
+    //Agrega un nodo al principio de la lista.
+    prepend(value){
+        const firstNode = new Node(value);
+        this.head.prev = firstNode; 
+        firstNode.next = this.head; 
+        this.head = firstNode; 
+        this.length++; 
+        return this; 
+        }
+
+    //Agrega un nodo en cualquier parte de la lista:
+    insert(value, index){
+        if(index >= this.length){
+            return this.append(value);
+        }
+        if(index <= 0){
+            return this.prepend(value);
+        }
+        const node = new Node(value); 
+        const firstPointer = this.getTheIndex(index - 1);
+        const holdingPointer = firstPointer.next;
+        firstPointer.next = node;
+        node.next = holdingPointer;
+        node.prev = firstPointer;
+        holdingPointer.prev = node;
+        this.length++;
+        return this; 
+    }
 }
  let doubleList = new MyDoubleLinkedList(1);
 console.log(doubleList); 
+console.log(doubleList.append(2));
+console.log(doubleList.append(3));
+//console.log(doubleList.prepend()); 
+console.log(doubleList.insert(2,4));
