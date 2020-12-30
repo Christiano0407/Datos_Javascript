2d0b05db400dd6f0ddf02302e8283e76eef68a2b
Christiano0407 <placebo0704@gmail.com>
2020-12-30 09:47:42 -0600

    remove-data

--- src2/js/hashTAble.js
+++ src2/js/hashTAble.js
@@ -17,7 +17,7 @@ class HashTable {
         return hash; 
     }
     //métodos:
-    //agregar:
+    //agregar datos:
     set(key, value){
     const address = this.hashMethod(key);
     if(!this.data[address]){
@@ -26,7 +26,7 @@ class HashTable {
     this.data[address].push([key, value]);
     return this.data; 
     }
-    //Búsqueda o obtener:
+    //Búsqueda o obtener o acceder por key y me regrese el valor.:
     get(key){
     const address = this.hashMethod(key); 
     const currentBucket = this.data[address]; 
@@ -39,6 +39,29 @@ class HashTable {
       }
     return undefined; 
     }
+    getKeys(){
+    return this.data.reduce((instance, value) => {
+        const keys = value.map(([key]) => key);
+        return instance.concat(keys)
+    }, []); 
+    }
+    //map = crea un nuevo array.
+    //reduce(ejecuta una función reductora sobre cada elemento de un array)
+
+    remove(key){
+        const address = this.hashMethod(key); 
+        const currentBucket = this.data[address]
+         if(currentBucket){
+             for(let i = 0; i < currentBucket.length; i++){
+                 if(currentBucket[i][0] === key){
+                     const element = currentBucket[i][0];
+                     delete currentBucket[i][1];
+                     return element; 
+                 }
+             }
+         }
+         return undefined
+    }
 }
 //Instancias
 const myHashTable = new HashTable(50);
@@ -46,5 +69,8 @@ console.log(myHashTable);
 console.log(myHashTable.set("Pammela",1992));
 console.log(myHashTable.set("Luisa",1991));  
 console.log(myHashTable.set("Mauricio",1990));
+console.log(myHashTable.set("Ivan", 1989)); 
 console.log(myHashTable.get("Pammela")); 
-console.log(myHashTable); 
+console.log(myHashTable.getKeys()); 
+console.log(myHashTable.remove("Ivan")); 
+console.log(myHashTable.getKeys()); 
