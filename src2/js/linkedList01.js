d3ff7e0f7b3eaf4f75e5c246750b6203f15aa83d
Christiano0407 <placebo0704@gmail.com>
2021-01-04 07:47:31 -0600

    one commit 2021

--- src2/js/linkedList01.js
+++ src2/js/linkedList01.js
@@ -31,7 +31,29 @@ class MySinglyList {
 
        this.length = 1; 
     }
+
+    append(value){
+        const newNode = new Node (value); 
+
+        this.tail.next = newNode; 
+        this.tail = newNode; 
+        this.length; 
+        return this; 
+    }
+
+    prepend(value){
+        const newNode = new Node(value); 
+        
+        newNode.next = this.head;
+        this.head = newNode; 
+        this.length++;
+
+        return this; 
+    }
 }
 
-let mySinglyList = new MySinglyList(1); 
-console.log(mySinglyList); 
+let myLinkedList = new MySinglyList(1); 
+console.log(myLinkedList); 
+console.log(myLinkedList.append(2)); 
+console.log(myLinkedList.append(3)); 
+console.log(myLinkedList.prepend(0)); 
