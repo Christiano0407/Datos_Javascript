809b1cf4c4f3607187cffb23833bbd50a16a7697
Christiano0407 <placebo0704@gmail.com>
2021-01-04 16:44:31 -0600

    Doubly linked

---
+++ src2/js/doubleLink.js
@@ -0,0 +1,6 @@
+// Doubly Linked List: 
+/* Referencia al nodo siguiente (next).
+Referencia al nodo anterior (prev).
+Valor del Nodo Dato que guarda el nodo ya sea un string, number, boolean, etc. (value).
+Referencia al primer nodo (head).
+Referencia al último nodo (tail). */
