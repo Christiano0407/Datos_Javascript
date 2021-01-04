aad0bc81f4839cfbb4faf85a6fc9eb8eacbd7d57
Christiano0407 <placebo0704@gmail.com>
2021-01-04 16:06:05 -0600

    insert

--- src2/js/linkedList01.js
+++ src2/js/linkedList01.js
@@ -50,6 +50,36 @@ class MySinglyList {
 
         return this; 
     }
+
+    insert (index, value) {
+        if(index >= this.length){
+            return this.append(value)
+        }
+
+        if(index === 0 ){
+            return this.prepend(value); 
+        }
+        
+        const newNode = new Node(value); 
+        const firstPointer = this.getTheIndex(index - 1); 
+        const holdingPointer = firstPointer.next; 
+        firstPointer.next = newNode; 
+        newNode.next = holdingPointer; 
+
+        this.length; 
+        return this; 
+    }
+    getTheIndex(index){
+        let counter = 0;
+        let currentNode = this.head;
+        
+        while(counter !== index){
+            currentNode = currentNode.next; 
+            counter++; 
+        }
+        return currentNode; 
+    }
+
 }
 
 let myLinkedList = new MySinglyList(1); 
@@ -57,3 +87,5 @@ console.log(myLinkedList);
 console.log(myLinkedList.append(2)); 
 console.log(myLinkedList.append(3)); 
 console.log(myLinkedList.prepend(0)); 
+console.log(myLinkedList.insert(2, 5)); 
+console.log(myLinkedList.insert(0, 1)); 
