94118da5f47e17ec0d2193995eda62ad721de813
Christiano0407 <placebo0704@gmail.com>
2021-01-05 10:11:29 -0600

    finished-doubly

--- src2/js/doubleLink.js
+++ src2/js/doubleLink.js
@@ -36,7 +36,7 @@ class myDoublyLinkedList {
     }
 
     prepend(value){
-    const newNode = new Node(value)
+    const newNode = new Node(value); 
 
     newNode.next = this.head; 
     this.head = newNode; 
@@ -46,10 +46,81 @@ class myDoublyLinkedList {
     return this; 
     }
 
+    insert(index, value){
+        if(index >= this.length){
+            return this.append(value); 
+        }
+        if(index === 0){
+            return this.prepend(value); 
+        }
+        const newNode = new Node(value);
+        const firstPointer = this.getTheIndex(index -1); 
+        newNode.prev = firstPointer; 
+        const holdingPointer = firstPointer.next; 
+        holdingPointer.prev = newNode;  
+        firstPointer.next = newNode; 
+        newNode.next = holdingPointer; 
+        this.length++; 
+
+        return this; 
+    }
+    getTheIndex(index){
+        let counter = 0; 
+        let currentNode = this.head; 
+
+        while(counter !== index){
+            currentNode = currentNode.next; 
+            counter++; 
+        }
+        return currentNode; 
+    }
+
+    unshift(value){
+        const newNode = new Node(value); 
+        
+        if(this.head){
+        this.head.prev = newNode; 
+        }else {
+            this.tail = newNode; 
+        }
+        this.head = newNode; 
+    }
+    push(value){
+
+    }
+
+    /* remove(index){
+        if(index >= this.length) {
+          console.error("index is out of limits of the array");
+        } else if( index == 0) {
+          this.head = this.head.next;
+          this.head.prev = null;
+          this.length--
+        }
+        
+        else if(index  === this.length - 1){
+          const firstPointer = this.getTheIndex(index - 1);
+          firstPointer.next = null;
+          
+          this.tail = firstPointer;
+          this.length--;
+        } else {
+          const firstPointer = this.getTheIndex(index - 1);
+          const pointerToRemove = firstPointer.next;
+          const nextToPointerToRemove = pointerToRemove.next;
+          nextToPointerToRemove.prev = firstPointer;
+          firstPointer.next = nextToPointerToRemove;
+          this.length--;
+        }
+       } */
+
 }
 
-let Doubly = new myDoublyLinkedList(1); 
-console.log(Doubly); 
-console.log(Doubly.append(2));
-console.log(Doubly.append(3));  
-console.log(Doubly.prepend(0)); 
+let doublyLink = new myDoublyLinkedList(1); 
+console.log(doublyLink); 
+console.log(doublyLink.append(2));
+console.log(doublyLink.append(3));  
+console.log(doublyLink.prepend(0));
+//console.log(doublyLink.unshift(4)); 
+console.log(doublyLink.insert(2, 4))
+//console.log(doublyLink.remove(1))
