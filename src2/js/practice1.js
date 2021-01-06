e41fa06d405b500dd61c6b5598312d0eb6758345
Christiano0407 <placebo0704@gmail.com>
2021-01-06 08:39:45 -0600

    all value methods

--- src2/js/practice1.js
+++ src2/js/practice1.js
@@ -69,6 +69,90 @@ class DoublyLinkedList{
     this.length++; 
     return shiftedNode; 
     }
+    //return node at given index
+    //return node at given index
+  getNodeAtIndex(index) {
+    //if index is not within list return null
+    if (index >= this.length || index < 0) return null;
+    //iterate through nodes until finding the one at index
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentIndex !== index) {
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+    return currentNode;
+  }
+
+  //change the node at the given index
+  setNodeAtIndex(val, index) {
+    //find the node using already built method
+    const foundNode = this.getNodeAtIndex(index);
+    //if the node is found update and return
+    if (foundNode) {
+      foundNode.value = val;
+      return foundNode;
+    }
+    //else return null
+    return null;
+  }
+
+  //insert a new node at the index with the given value
+  insertAtIndex(index, val) {
+    //if index not valid return false
+    if (index < 0 || index > this.length) {
+      return false;
+    }
+    //if index is zero use already built unshift
+    if (index === 0) {
+      return this.unshift(val);
+    }
+    //if index is at end of list, use already built push
+    if (index === this.length) {
+      return this.push(val);
+    }
+    // make a new node, find the nodes before and after it, make before's next the new node, and the newNode's next the after
+    const newNode = new Node(val);
+    const after = this.getNodeAtIndex(index);
+    const before = this.getNodeAtIndex(index - 1);
+    newNode.next = after;
+    before.next = newNode;
+
+    this.length++;
+    return this;
+  }
+    //Remove node from index
+    removeFrom(index) {
+        //if index is invalid return false
+        if (index < 0 || index >= this.length) {
+          return false;
+        }
+        //if index is at beginning use already built shift
+        if (index === 0) return this.shift();
+        //if index is at end use already built pop
+        if (index === this.length - 1) return this.pop();
+        //get before and after, set before's next to after and remove after's reference from removed Node
+        const before = this.getNodeAtIndex(index - 1);
+        const removedNode = this.getNodeAtIndex(index);
+        before.next = removedNode.next;
+        removedNode.next = null;
+    
+        this.length--;
+        return removedNode;
+      }
+    printList(){
+        console.log(list)
+        if(this.head){
+        let current = this.head; 
+        while(current.next){
+            console.log(current);
+            current = current.next; 
+        }
+        console.log(current);
+        }else {
+            console.log("Empty List"); 
+        }
+    }
 
 }
 
@@ -82,3 +166,5 @@ console.log(myLinked.unshift("Melón"));
 console.log(myLinked.unshift("Papaya")); 
 console.log(myLinked.shift(1)); 
 console.log(myLinked); 
+console.log(myLinked.removeFrom(2));
+console.log(myLinked);
